79b7006384bf13063d6da2d6d218c88bceeb7c5c
rtorruellas <richard.torruellas@sony.com>
2021-11-05 10:51:56 -0700

    foo

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,6 +1,17 @@
+const path = require("path");
+const fs = require("fs");
+
+const d = "./dist";
+
 exports.handler = async (event) => {
   const subject = event.queryStringParameters.name || "World";
   console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  try {
+    const files = await fs.readdir(d);
+    for (const file of files) console.log(file);
+  } catch (err) {
+    console.error(err);
+  }
   return {
     statusCode: 200,
     body: `Hello ${subject}!`,
