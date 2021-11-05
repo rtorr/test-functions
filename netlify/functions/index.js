61a9cc1a11a59df1358b22c7778ab7ecdf662f89
rtorruellas <richard.torruellas@sony.com>
2021-11-05 12:38:06 -0700

    foo

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -6,34 +6,30 @@ exports.handler = async (event) => {
   const subject = event.queryStringParameters.name || "World";
   console.log("coooooooooool", sites);
   let res = "";
+  const site = sites[Math.floor(Math.random() * items.length)];
   try {
     const req = await new Promise((resolve, reject) => {
       https
-        .get(
-          `https://quirky-northcutt-01be8c.netlify.app/${
-            sites[Math.floor(Math.random() * items.length)]
-          }`,
-          (resp) => {
-            let data = "";
+        .get(`https://quirky-northcutt-01be8c.netlify.app/${site}`, (resp) => {
+          let data = "";
 
-            // A chunk of data has been received.
-            resp.on("data", (chunk) => {
-              data += chunk;
-            });
+          // A chunk of data has been received.
+          resp.on("data", (chunk) => {
+            data += chunk;
+          });
 
-            // The whole response has been received. Print out the result.
-            resp.on("end", () => {
-              resolve(data);
-            });
-          }
-        )
+          // The whole response has been received. Print out the result.
+          resp.on("end", () => {
+            resolve(data);
+          });
+        })
         .on("error", (err) => {
           reject("Error: " + err.message);
         });
     });
     res = req;
   } catch (e) {}
-
+  console.log("nice", res, site);
   return {
     statusCode: 200,
     body: res,
