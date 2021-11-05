0491df64615c87e2daa4a6c51caa179b5d403efd
rtorruellas <richard.torruellas@sony.com>
2021-11-05 11:42:38 -0700

    foo

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -2,9 +2,8 @@ const lang = "index";
 
 exports.handler = async (event) => {
   const foo = require(`./sites/${lang}.html`);
-  console.log(foo);
   return {
     statusCode: 302,
-    body: `Hello ${subject}!`,
+    body: foo,
   };
 };
