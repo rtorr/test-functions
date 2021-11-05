263991a305f7c5393b3e491066dc1580769ebb16
rtorruellas <richard.torruellas@sony.com>
2021-11-05 11:56:04 -0700

    foo

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,11 +1,8 @@
-const fs = require("fs");
-
-const lang = "index";
-
 exports.handler = async (event) => {
-  const foo = fs.readFileSync(require.resolve(`./sites/${lang}.html`));
   return {
     statusCode: 302,
-    body: `${foo}`,
+    headers: {
+      location: "https://www.google.com/",
+    },
   };
 };
