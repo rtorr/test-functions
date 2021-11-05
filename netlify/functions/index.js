fdfcdf69f75ea60eda3b0d0c1cdf4ff01489cefd
rtorruellas <richard.torruellas@sony.com>
2021-11-05 12:07:15 -0700

    foo

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,8 +1,28 @@
+const https = require("https");
+
 exports.handler = async (event) => {
+  const req = await new Promise((resolve, reject) => {
+    https
+      .get("quirky-northcutt-01be8c.netlify.app/one", (resp) => {
+        let data = "";
+
+        // A chunk of data has been received.
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on("end", () => {
+          console.log(JSON.parse(data));
+        });
+      })
+      .on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+  });
+
   return {
-    statusCode: 302,
-    headers: {
-      location: "https://www.google.com/",
-    },
+    statusCode: 200,
+    body: "hello",
   };
 };
