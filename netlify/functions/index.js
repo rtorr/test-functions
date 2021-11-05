c076ba759eee08b7b8f0dd3a5312e34c8ca90575
rtorruellas <richard.torruellas@sony.com>
2021-11-05 10:57:50 -0700

    foo

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,13 +1,24 @@
 const path = require("path");
-const fs = require("fs/promises");
+const fs = require("fs");
 
 const d = "./dist";
+
+const read = function (dir) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dir, (err, files) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(files);
+    });
+  });
+};
 
 exports.handler = async (event) => {
   const subject = event.queryStringParameters.name || "World";
   console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
   try {
-    const files = await fs.readdir(d);
+    const files = await read(d);
     for (const file of files) console.log(file);
   } catch (err) {
     console.error(err);
