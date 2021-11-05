3a62bae35994448012288b793c170d440b32c908
rtorruellas <richard.torruellas@sony.com>
2021-11-05 11:25:10 -0700

    foo

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,31 +1,12 @@
 const path = require("path");
 const fs = require("fs");
-
-const d = "./../";
-
-const read = function (dir) {
-  return new Promise((resolve, reject) => {
-    fs.readdir(dir, (err, files) => {
-      console.log(err, files);
-      if (err) {
-        reject(err);
-      }
-      resolve(files);
-    });
-  });
-};
+const sites = require("./sites.json");
 
 exports.handler = async (event) => {
-  const subject = event.queryStringParameters.name || "World";
-  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-  try {
-    const files = await read(d);
-    for (const file of files) console.log(file);
-  } catch (err) {
-    console.error(err);
-  }
   return {
-    statusCode: 200,
-    body: `Hello ${subject}!`,
+    statusCode: 301,
+    headers: {
+      location: "https://www.google.com/",
+    },
   };
 };
